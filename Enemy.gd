5fac0f07eb53e399489f632beb84a0beecba81f1
Ben Counsell <bencounsell@hotmail.co.uk>
2023-11-17 13:15:56 +0000

    got rudimentary enemy movement

--- Enemy.gd
+++ Enemy.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-var speed = 25
+var speed = 100
 var player_chase = false
 var player = null
 
@@ -9,11 +9,12 @@ func _ready():
 
 func _physics_process(delta):
 	if player_chase:
-		position += (player.position - position)/speed
+		position += (player.position - position) / speed
+
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
-	
+
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
